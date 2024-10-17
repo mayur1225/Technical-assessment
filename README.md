7d2bef57aaa50a0f971259ff845bd8e3b80b46d6
Mayur Patel <patelmayur2591@gmail.com>
2024-10-17 10:27:14 +0530

    Add test script file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Technical-assessment
 user authentication in Node.js using Test-Driven Development (TDD)
+
+# Install node dependencies
+npm i
+
+# RUN THE APP
+npm start
+
+# RUN THE TEST SCRIPT 
+npm test
---
+++ src/tests/auth.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, beforeAll, afterAll } from '@jest/globals';
+import request from 'supertest';
+import app from '../app';
+import mongoose from 'mongoose';
+
+describe('User Authentication', () => {
+    beforeAll(async () => {
+        const mongoUri = process.env.MONGO_URI;
+        if (typeof mongoUri === 'string') {
+            await mongoose.connect(mongoUri);
+        } else {
+            throw new Error('MONGO_URI is not defined');
+        }
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+    });
+
+    it('should register a new user', (done) => {
+        request(app)
+            .post('/auth/register')
+            .send({ username: 'mayur.patel', password: 'Mayur@2024' })
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.message).toBe('User registered successfully');
+                done();
+            });
+    });
+});
